e549c8d75c7e06d94ccf7581e7f7d8c29a872092
Елена <simonaitene@yandex.ru>
2023-04-09 19:24:29 +0300

    Сделано ДЗ

---
+++ src/Bicycle.java
@@ -0,0 +1,9 @@
+public class Bicycle extends WheeledTransport {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void service(){
+        System.out.println(" Переворачиваем велосипед");
+        updateTyre();
+    }
+}
